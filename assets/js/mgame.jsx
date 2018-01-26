8261965be9e79ddca27789d1d311fc36b4e18691
Rajdeep <rajdeepkaur2nov@gmail.com>
2018-01-26 15:23:38 -0500

    update

--- assets/js/mgame.jsx
+++ assets/js/mgame.jsx
@@ -4,13 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from 'reactstrap';
 
-export default function run_demo(root) {
+export default function run_game(root) {
   ReactDOM.render(<MGame />, root);
 }
 
 // App state for Game is:
 // {
 //    prevTile: String    // prev tile value
+//    letters: Array     // Values of tiles displayed
 // }
 //
 
@@ -18,9 +19,11 @@ class MGame extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      prevTile: null
+      prevTile: null,
+      letters: ['A1','B1','C1','D1','E1','F1','G1','H1','A2','B2','C2','D2','E2','F2','G2','H2']
     };
     this.setNewState = this.setNewState.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   // update the state of game
@@ -33,126 +36,97 @@ class MGame extends React.Component {
 
   // Reset the game
   reset(){
+    let new_letters = _.shuffle(this.state.letters);
+    let st2 = _.extend(this.state, {
+      prevTile: null,
+      letters: new_letters
+    });
+    this.setState(st2);
+    this.state.letters.map((letter) => {
+      document.getElementById(letter).disabled = false;
+      document.getElementById(letter).innerHTML = "";
+    });
     document.getElementById("totalClicks").innerHTML=0;
-    var letters = ("ABCDEFGH").split('');
-    letters.map((letter) => {
-      let v1 = document.getElementById(letter+"1");
-      let v2 = document.getElementById(letter+"2");
-      v1.innerHTML = ""; v2.innerHTML = "";
-      v1.disabled = false; v2.disabled = false;
-    }
-  );
-}
+  }
 
-render(){
-  return(
-    <div id="blocks" className="blocks">
-    <div id="board"><Board root={this}/></div>
-    <div id="clicks">
-    Total Clicks: <span id="totalClicks">0</span>
-    </div>
-    <div>
-    <button className="btn-default" onClick={this.reset}>Restart</button>
-    </div>
-    </div>
-  );
-}
-}
-
-function Board(props) {
-  return(
-    <div>
-    <div>
-    <Tiles name="A" id="A1" root={props.root}/>
-    <Tiles name="B" id="B1" root={props.root}/>
-    <Tiles name="G" id="G1" root={props.root}/>
-    <Tiles name="D" id="D1" root={props.root}/>
-    </div>
-    <div>
-    <Tiles name="F" id="F1" root={props.root}/>
-    <Tiles name="H" id="H1" root={props.root}/>
-    <Tiles name="C" id="C1" root={props.root}/>
-    <Tiles name="E" id="E1" root={props.root}/>
-    </div>
-    <div>
-    <Tiles name="A" id="A2" root={props.root}/>
-    <Tiles name="D" id="D2" root={props.root}/>
-    <Tiles name="G" id="G2" root={props.root}/>
-    <Tiles name="F" id="F2" root={props.root}/>
-    </div>
-    <div>
-    <Tiles name="E" id="E2" root={props.root}/>
-    <Tiles name="H" id="H2" root={props.root}/>
-    <Tiles name="B" id="B2" root={props.root}/>
-    <Tiles name="C" id="C2" root={props.root}/>
-    </div>
-    </div>
-  );
+  render(){
+    return(
+      <div id="blocks" className="blocks">
+      <div id="board">
+      <div>
+      <Tiles id={this.state.letters[0]} root={this}/>
+      <Tiles id={this.state.letters[1]} root={this}/>
+      <Tiles id={this.state.letters[2]} root={this}/>
+      <Tiles id={this.state.letters[3]} root={this}/>
+      </div>
+      <div>
+      <Tiles id={this.state.letters[4]} root={this}/>
+      <Tiles id={this.state.letters[5]} root={this}/>
+      <Tiles id={this.state.letters[6]} root={this}/>
+      <Tiles id={this.state.letters[7]} root={this}/>
+      </div>
+      <div>
+      <Tiles id={this.state.letters[8]} root={this}/>
+      <Tiles id={this.state.letters[9]} root={this}/>
+      <Tiles id={this.state.letters[10]} root={this}/>
+      <Tiles id={this.state.letters[11]} root={this}/>
+      </div>
+      <div>
+      <Tiles id={this.state.letters[12]} root={this}/>
+      <Tiles id={this.state.letters[13]} root={this}/>
+      <Tiles id={this.state.letters[14]} root={this}/>
+      <Tiles id={this.state.letters[15]} root={this}/>
+      </div>
+      </div><br/>
+      <div id="clicks">
+      Total Clicks: <span id="totalClicks">0</span>
+      </div>
+      <div>
+      <button className="btn-default" onClick={this.reset}>Restart</button>
+      </div>
+      </div>
+    );
+  }
 }
 
 function Tiles(props) {
-
-  // Runs after Timeout of 1sec in case of mismatch
-  function setTimer(e,prev1,prev2){
-    props.root.setNewState(null);
-    if(prev1.disabled){
-      console.log("RIGHT 1");
-      prev1.innerHTML="";
-      prev1.disabled=false;
-    }
-    if(prev2.disabled){
-      console.log("RIGHT 2");
-      prev2.innerHTML="";
-      prev2.disabled=false;
-    }
-    e.target.innerHTML="";
-    e.target.disabled=false;
-    console.log("DIFFERENT");
-  }
-
   function checkTiles(e){
-    let current = props.root.state;
-    let totalClicksValue = document.getElementById("totalClicks");
-    totalClicksValue.innerHTML=Number(totalClicksValue.innerHTML)+1;
-    if(current.prevTile==null){
-      props.root.setNewState(props.name);
-      e.target.innerHTML=props.name;
-      e.target.disabled=true;
-    }
-    else if(current.prevTile==props.name){
-      e.target.innerHTML=props.name;
-      e.target.disabled=true;
-      props.root.setNewState(null);
-      console.log("SAME");
-    }
-    else{
-      document.getElementById('blocks').style.pointerEvents = 'none';
-      e.target.innerHTML=props.name;
-      e.target.disabled=true;
-      let prev1=document.getElementById(current.prevTile+"1");
-      let prev2=document.getElementById(current.prevTile+"2");
+    if(props.root.state.prevTile!=e.target.id){
+      //Increment Clicks
+      let totalClicksValue = document.getElementById("totalClicks");
+      totalClicksValue.innerHTML=Number(totalClicksValue.innerHTML)+1;
+      let previous = props.root.state.prevTile;
       let curr=e.target;
-      setTimeout(() => {
+      //Update Selected Tile
+      curr.innerHTML=curr.id.charAt(0);
+      // Check if Tile Letter is same as previous
+      if(previous==null){
+        props.root.setNewState(curr.id);
+      }
+      else if(previous.charAt(0)==curr.id.charAt(0)){
         props.root.setNewState(null);
-        if(prev1.disabled){
-          console.log("RIGHT 1");
-          prev1.innerHTML="";
-          prev1.disabled=false;
-        }
-        if(prev2.disabled){
-          console.log("RIGHT 2");
-          prev2.innerHTML="";
-          prev2.disabled=false;
-        }
-        curr.innerHTML="";
-        curr.disabled=false;
-        console.log("DIFFERENT");
-        document.getElementById('blocks').style.pointerEvents = 'auto';
-      }, 1000)
-
+        document.getElementById('board').style.pointerEvents = 'none';
+        console.log("SAME");
+        setTimeout(() => {
+          curr.innerHTML="";
+          curr.disabled=true;
+          document.getElementById(previous).innerHTML="";
+          document.getElementById(previous).disabled=true;
+          document.getElementById('board').style.pointerEvents = 'auto';
+        },1000);
+      }
+      else{
+        document.getElementById('board').style.pointerEvents = 'none';
+        setTimeout(() => {
+          props.root.setNewState(null);
+          document.getElementById(previous).innerHTML="";
+          curr.innerHTML="";
+          console.log("DIFFERENT");
+          document.getElementById('board').style.pointerEvents = 'auto';
+        }, 1000);
+      }
     }
   }
-
   return(
     <button className="btn btn-primary" id={props.id} onClick={checkTiles} />
   );
